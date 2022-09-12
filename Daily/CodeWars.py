345e51ebe8624c42391330a5f7d97c0fefcb9dfd
DmitryLurk <redalert337596@gmail.com>
2022-09-12 15:41:34 +0400

    Add square kata

--- Daily/CodeWars.py
+++ Daily/CodeWars.py
@@ -16,6 +16,7 @@ def high(x):
 
 high('take me to semynak')
 
+
 def likes(names):
     if len(names) == 0:
         return 'no one likes this'
@@ -26,5 +27,17 @@ def likes(names):
     elif len(names) == 3:
         return f'{names[0]}, {names[1]} and {names[2]} like this'
     else:
-        return f'{names[0]}, {names[1]} and {len(names)-2} others like this'
+        return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
+
+
 likes(['Max', 'John', 'Mark'])
+
+
+def square_digits(num):
+    '''square every digit of a number and concatenate them.
+    For example, if we run 9119 through the function, 811181
+    will come out, because 92 is 81 and 12 is 1.'''
+    return print(int(''.join(str(pow(int(i), 2)) for i in str(num))))
+
+
+square_digits(9119)
